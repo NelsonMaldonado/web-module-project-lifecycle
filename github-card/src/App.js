df0f37b7aaaf8c5a90f76f2bc3c40f78dd6d237b
Nelson Maldonado <play2nino@gmail.com>
2021-09-28 23:31:44 -0700

    api getch

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,11 +1,22 @@
-import logo from "./logo.svg"
+import React from "react"
+import axios from "axios"
 import "./App.css"
 
 class App extends React.Component {
+  state = {
+    name: "Audi",
+  }
+
+  //Fetching data
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then((res) => {
+      console.log(res)
+    })
+  }
   render() {
     return (
       <div className="App">
-        <h1>GitHub API</h1>
+        <h1>GitHub API 2.0 by Nelly</h1>
       </div>
     )
   }
