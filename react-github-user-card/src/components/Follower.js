49b0a52855a635ac82c54ed3e3379d294087c98b
Nelson Maldonado <play2nino@gmail.com>
2021-10-27 00:32:28 -0500

    changing from functional to class component

--- react-github-user-card/src/components/Follower.js
+++ react-github-user-card/src/components/Follower.js
@@ -1,10 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import User from "./User"
 
 const Follower = () => {
+  const [follower, setFollower] = useState({
+    avatar_url: "load image",
+    login: "load name",
+    id: "load id",
+  })
+  console.log(follower)
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/NelsonMaldonado/followers")
+      .then((res) => {
+        setFollower({
+          ...User,
+          avatar_url: res.data.avatar_url,
+          login: res.data.login,
+          id: res.data.id,
+        })
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <div>
-      <div>Follower Name:</div>
-      <div>Follower Image</div>
+      <div>Image</div>
+      {follower && follower.map((item) => <div>{item.login}</div>)}
       Follower
     </div>
   )
