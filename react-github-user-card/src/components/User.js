b823660fadeedb5b874588e95645e6969e266e28
Nelson Maldonado <play2nino@gmail.com>
2021-10-26 23:56:01 -0500

    data fetched successfully

--- react-github-user-card/src/components/User.js
+++ react-github-user-card/src/components/User.js
@@ -2,13 +2,26 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const User = () => {
-  const [user, setUser] = useState()
-
+  const [user, setUser] = useState({
+    name: "loading",
+    bio: "loading",
+    public_repos: "loading",
+    followers: "loading",
+    id: "loading",
+  })
+  console.log(user)
   useEffect(() => {
     axios
-      .get("https://api.github.com/NelsonMaldonado/")
+      .get("https://api.github.com/users/NelsonMaldonado")
       .then((res) => {
-        console.log(res)
+        setUser({
+          ...user,
+          name: res.data.name,
+          bio: res.data.bio,
+          public_repos: res.data.public_repos,
+          followers: res.data.followers,
+          id: res.data.id,
+        })
       })
       .catch((err) => {
         console.log(err)
