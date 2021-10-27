e121e17ca65d7b8e63193f0cd4a0567be351bb23
Nelson Maldonado <play2nino@gmail.com>
2021-10-26 23:30:56 -0500

    User and Follower list component on the DOM

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg"
 import "./App.css"
+import User from "./components/User"
+import FollowerList from "./components/FollowerList"
 
 function App() {
   return (
     <div className="App">
       <h1>GitHub Card</h1>
+      <User />
+      <FollowerList />
     </div>
   )
 }
---
+++ react-github-user-card/src/components/FollowerList.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const FollowerList = () => {
+  return <div>Follower List</div>
+}
+
+export default FollowerList
